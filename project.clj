f6d832d515369cb624f00ad3dd1239f96868ba8c
Michael Cohen <mcohen01@gmail.com>
2014-01-20 17:13:10 -0800

    update undertow dep

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ring-undertow-adapter "0.1.2"
+(defproject org.clojars.mcohen01/ring-undertow-adapter "0.1.2"
   :description "Ring Underow adapter."
   :url "http://github.com/piranha/ring-adapter-undertow"
   :license {:name "ISC License"
