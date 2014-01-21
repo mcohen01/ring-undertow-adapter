89a1055bab48dbf0db35452fc846770f8db386e3
Michael Cohen <mcohen01@gmail.com>
2014-01-20 17:09:10 -0800

    update undertow dep

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "http://github.com/piranha/ring-adapter-undertow"
   :license {:name "ISC License"
             :url "http://opensource.org/licenses/ISC"}
-  :dependencies [[io.undertow/undertow-core "1.0.0.Beta8"]]
+  :dependencies [[io.undertow/undertow-core "1.0.0.Beta31"]]
   :profiles {:dev {:dependencies [[clj-http "0.7.6"]]}})
